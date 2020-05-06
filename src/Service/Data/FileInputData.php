e224feda747b0e040a6d06d433ac18845e1945fb
Karp <karp@karp.space>
2020-05-06 15:35:22 +0100

    Add support for both lower and upper case in url

--- src/Service/Data/FileInputData.php
+++ src/Service/Data/FileInputData.php
@@ -47,10 +47,10 @@ class FileInputData implements InputDataInterface
             $availableCameras = [];
 
             foreach ($rover['cameras'] as $camera) {
-                $availableCameras[$camera] = $this->cameras[$camera];
+                $availableCameras[strtoupper($camera)] = $this->cameras[$camera];
             }
 
-            $this->rovers[$name] = RoverFactory::create(
+            $this->rovers[strtoupper($name)] = RoverFactory::create(
                 $name,
                 $availableCameras,
                 DateTimeImmutable::createFromFormat(
